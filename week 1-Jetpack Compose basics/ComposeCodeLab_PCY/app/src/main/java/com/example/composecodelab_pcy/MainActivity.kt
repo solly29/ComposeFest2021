30edf24537a3fd8c32f28f122fec358f94cc32b8
solly29 <cksdud1311@naver.com>
2021-11-01 22:29:09 +0900

    jetpack compose basics 9장
    rememberSaveable

--- week 1-Jetpack Compose basics/ComposeCodeLab_PCY/app/src/main/java/com/example/composecodelab_pcy/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeCodeLab_PCY/app/src/main/java/com/example/composecodelab_pcy/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -80,7 +81,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 @Composable
 fun MyApp() {
 
-    var shouldShowOnboarding by remember {
+    var shouldShowOnboarding by rememberSaveable {
         mutableStateOf(true)
     }
 
