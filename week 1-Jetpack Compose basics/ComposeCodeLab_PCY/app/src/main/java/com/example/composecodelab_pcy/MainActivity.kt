0d5a377b3a480a7582d219d9bdb52d555a05c700
solly29 <cksdud1311@naver.com>
2021-11-01 22:24:32 +0900

    jetpack compose basics 9장
    LazyColumn

--- week 1-Jetpack Compose basics/ComposeCodeLab_PCY/app/src/main/java/com/example/composecodelab_pcy/MainActivity.kt
+++ week 1-Jetpack Compose basics/ComposeCodeLab_PCY/app/src/main/java/com/example/composecodelab_pcy/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -92,9 +94,9 @@ fun MyApp() {
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        names.forEach {
+fun Greetings(names: List<String> = List(1000) {"$it"}) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) {
             Greeting(name = it)
         }
     }
