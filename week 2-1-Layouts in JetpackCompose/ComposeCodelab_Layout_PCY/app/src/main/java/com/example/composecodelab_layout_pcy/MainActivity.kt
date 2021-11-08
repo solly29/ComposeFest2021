229f406cfe0eefca818a6e8b966d491afa5f95f2
solly29 <cksdud1311@naver.com>
2021-11-08 20:15:23 +0900

    2021-11-08 gradle 수정

--- week 2-1-Layouts in JetpackCompose/ComposeCodelab_Layout_PCY/app/src/main/java/com/example/composecodelab_layout_pcy/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeCodelab_Layout_PCY/app/src/main/java/com/example/composecodelab_layout_pcy/MainActivity.kt
@@ -17,22 +17,17 @@ class MainActivity : ComponentActivity() {
             ComposeCodelab_Layout_PCYTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeCodelab_Layout_PCYTheme {
-        Greeting("Android")
+
     }
 }
