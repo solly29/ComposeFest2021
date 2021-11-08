daa1ea9bb86071a6da70905ff436f5542982fd22
solly29 <cksdud1311@naver.com>
2021-11-08 20:38:16 +0900

    2021-11-08 PhotographerCard

--- week 2-1-Layouts in JetpackCompose/ComposeCodelab_Layout_PCY/app/src/main/java/com/example/composecodelab_layout_pcy/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeCodelab_Layout_PCY/app/src/main/java/com/example/composecodelab_layout_pcy/MainActivity.kt
@@ -3,11 +3,23 @@ package com.example.composecodelab_layout_pcy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composecodelab_layout_pcy.ui.theme.ComposeCodelab_Layout_PCYTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,12 +28,51 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeCodelab_Layout_PCYTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-
-                }
+                PhotographerCard()
             }
         }
     }
+}
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable {
+
+            }
+            .padding(16.dp)) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(
+                text = "Alfred Sisley",
+                fontWeight = FontWeight.Bold
+            )
+
+            // 불투명
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(
+                    text = "3 minutes ago",
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
@@ -29,5 +80,13 @@ class MainActivity : ComponentActivity() {
 fun DefaultPreview() {
     ComposeCodelab_Layout_PCYTheme {
 
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PhotographerCardPreview() {
+    ComposeCodelab_Layout_PCYTheme {
+        PhotographerCard()
     }
 }
