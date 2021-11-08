50f60b02ea343536b4eb7b537b9eefb4e9df0c03
solly29 <cksdud1311@naver.com>
2021-11-08 21:35:35 +0900

    2021-11-08 6장

--- week 2-1-Layouts in JetpackCompose/ComposeCodelab_Layout_PCY/app/src/main/java/com/example/composecodelab_layout_pcy/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeCodelab_Layout_PCY/app/src/main/java/com/example/composecodelab_layout_pcy/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.composecodelab_layout_pcy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,13 +14,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.composecodelab_layout_pcy.ui.theme.ComposeCodelab_Layout_PCYTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeCodelab_Layout_PCYTheme {
                 // A surface container using the 'background' color from the theme
-                LayoutsCodelab()
+                ScrollingList()
             }
         }
     }
@@ -55,15 +56,17 @@ fun LayoutsCodelab() {
             )
         }
     ) {
-        BodyContent(Modifier.padding(it).padding(8.dp))
+        BodyContent(
+            Modifier
+                .padding(it)
+                .padding(8.dp))
     }
 }
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+        ImageList()
     }
 }
 
@@ -72,6 +75,88 @@ fun BodyContent(modifier: Modifier = Modifier) {
 fun LayoutsCodelabPreview() {
     ComposeCodelab_Layout_PCYTheme {
         LayoutsCodelab()
+    }
+}
+
+@Composable
+fun SimpleList() {
+
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #$it")
+        }
+    }
+}
+
+@Composable
+fun LazyList() {
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            Text(text = "Item #$it")
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = rememberImagePainter(
+            data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList() {
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = "Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
     }
 }
 
