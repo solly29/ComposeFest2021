18cea85dca91c612f21bad77d6422a48364a1d1c
solly29 <cksdud1311@naver.com>
2021-11-08 20:10:33 +0900

    2021-11-08 compose layout project

---
+++ week 2-1-Layouts in JetpackCompose/ComposeCodelab_Layout_PCY/app/src/main/java/com/example/composecodelab_layout_pcy/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.composecodelab_layout_pcy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composecodelab_layout_pcy.ui.theme.ComposeCodelab_Layout_PCYTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCodelab_Layout_PCYTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeCodelab_Layout_PCYTheme {
+        Greeting("Android")
+    }
+}
